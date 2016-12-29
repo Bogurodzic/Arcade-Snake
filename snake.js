4ee040ee1fb9b5443fa8cdf566bd568d6081a3db
Kamil Jarzab <jarzbkamil@gmail.com>
2016-12-29 18:30:29 +0100

    Added death and score

--- snake.js
+++ snake.js
@@ -1,6 +1,7 @@
 var snake;
 var multiplier = 20;
 var food;
+var score;
 
 
 
@@ -8,6 +9,7 @@ function setup() {
 	createCanvas(600, 600);
 	frameRate(20);
 	snake = new Snake();
+	score = new Score();
 	location();
 }
 
@@ -15,7 +17,6 @@ function location() {
 	var columns = floor(width/multiplier);
 	var rows = floor(height/multiplier);
 	food = createVector(floor(random(columns)), floor(random(rows)));
-
 	food.mult(multiplier);
 	
 }
@@ -24,11 +25,17 @@ function draw() {
 	background(25);
 	snake.update();
 	snake.show();
+	snake.death();
+	console.log(snake.x);
+	console.log(snake.y);
 
 	if (snake.eat(food) === true){
+		score.points += 1;
 		location();
 	}
+	score.displayString();
 
+	score.displayScore();
 
 	fill(144, 0, 150);
 	rect(food.x, food.y, multiplier, multiplier);
@@ -36,14 +43,58 @@ function draw() {
 }
 
 function keyPressed() {
-	if (keyCode === UP_ARROW) {
-			snake.direction(0, -1)
-	} 	else if (keyCode === DOWN_ARROW) {
-			snake.direction(0, 1)
-	}	else if (keyCode === LEFT_ARROW) {
-			snake.direction(-1, 0)
-	}	else if (keyCode === RIGHT_ARROW) {
-			snake.direction(1, 0)
+	if (snake.size === 0) {
+
+		if (keyCode === UP_ARROW) {
+			snake.direction(0, -1);			
+		} 	else if (keyCode === DOWN_ARROW) {
+				snake.direction(0, 1)
+		}	else if (keyCode === LEFT_ARROW) {
+				snake.direction(-1, 0)
+		}	else if (keyCode === RIGHT_ARROW) {
+				snake.direction(1, 0)
+		}		
+	} else {
+		if (keyCode === UP_ARROW) {
+			if (snake.yspeed !== 1) {
+				snake.direction(0, -1);	
+			} else {
+				return;	
+			}
+		} 	else if (keyCode === DOWN_ARROW) {
+				if (snake.yspeed !== -1){
+					snake.direction(0, 1);
+			} else {
+				return;
+			}
+		}	else if (keyCode === LEFT_ARROW) {
+				if(snake.xspeed !== 1){
+					snake.direction(-1, 0)					
+				} else {
+					return;
+				}
+		}	else if (keyCode === RIGHT_ARROW) {
+				if(snake.xspeed !== -1){
+					snake.direction(1, 0);
+				} else {
+					return;
+				}
+		}		
+	}
+
+}
+
+function Score(){
+	this.points = 0;
+
+	this.displayString = function(){
+		textSize(10);
+		text("Score: ", 550, 0, 15, 15);
+	}
+
+	this.displayScore = function(){
+		textSize(10);
+		text(this.points , 585, 0, 15, 15);
 	}
 }
 
@@ -80,6 +131,25 @@ function Snake() {
 		}
 		rect(this.x, this.y, multiplier, multiplier);
 
+	}
+
+	this.death = function(){
+		for (var i=0; i<this.tail.length-1; i++){
+			var distance = dist(this.x, this.y, this.tail[i].x, this.tail[i].y);
+			if(distance < 1){
+				this.reset();
+			}
+		} 
+	}
+
+	this.reset = function(){
+		this.size = 0;
+		this.tail = [];
+		this.x=0;
+		this.y=0;
+		score.points = 0;
+		location();	
+		redraw();
 	}
 
 	this.direction = function(x, y) {
